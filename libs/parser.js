997f40ab4d7b7c7cd0bb8502135089f728069a5d
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-07-06 14:32:22 +0600

    init

--- libs/parser.js
+++ libs/parser.js
@@ -15,8 +15,7 @@ module.exports = function (url, main, title, link, file){
     link: link
     }])
     (function (err,data) {
-      data = data.slice(0, 1).replace(/[\[\]']+/g, '');
-
+      data = data.slice(0, 1)
       console.log(data);
       var params = {
           Bucket: 'newsbotkg',
