a114267ee9a039cf7aade80cc5a2352ea24335d4
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-07-08 22:25:23 +0600

    init

--- libs/job.js
+++ libs/job.js
@@ -15,6 +15,7 @@ function checkChanges() {
       svodka.One('azattyk.json'),
       svodka.One('knews.json')
     ]).then((files)=>{
+      console.log('files - ' + files);
       Promise.all([
         parser('https://ru.sputnik.kg/Kyrgyzstan/','.b-stories__item','.b-stories__title h2','a@href'),
         parser('https://24.kg/','.one','.title','.title a@href', '24.json'),
@@ -22,6 +23,7 @@ function checkChanges() {
         parser('https://rus.azattyk.org/z/3734','.media-block','h4','.content a@href'),
         parser('http://knews.kg/','.td-block-span4','h3','a@href')
       ]).then((parsers) =>{
+          console.log('parsers - ' + parsers);
           var file;
           var tosend = [];
           for (var i = 0; i < 4; i++) {
