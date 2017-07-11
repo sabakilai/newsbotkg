28b3a7da83e211956f61abf85158b23fad36c4d1
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-07-11 12:47:58 +0600

    init

--- libs/parser.js
+++ libs/parser.js
@@ -15,7 +15,7 @@ module.exports = function (url, main, title, link){
       if (err) {
         reject(err);
       }
-      console.log("parser data - " + data[0]);
+      console.log("parser data - " + JSON.stringify(data[0]));
       resolve((data[0]))
     })
   })
